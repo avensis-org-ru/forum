ab5a2de8fbde6492599f45a83b59c0daf831c735
levenkov <artem@levenkov.org>
2015-02-05 11:00:40 +0300

    Refactoring.

--- forum.rb
+++ forum.rb
@@ -1,27 +1,29 @@
 require 'sinatra'
 require 'mysql'
 
+
 $db = Mysql.connect 'pandora', 'avensis', '64bf7fdb', 'avensis'
 
-class Forums
+
+class Simpozium
   @db
 
   def initialize db
     @db = db
   end
 
-  def list
-    forums = {}
-    res = @db.query 'select `forum_id`, `forum_name` from `avensis_forums` where 1;'
+  def forum_indexes
+    ids = []
+    res = @db.query 'select `forum_id` from `avensis_forums` where 1;'
 
     res.each do |row|
-      forums[row[0]] = row[1]
+      ids << row[0]
     end
 
-    forums
+   ids 
   end
 
-  def get id
+  def forum id
     Forum.new @db, id
   end
 end
@@ -41,15 +43,41 @@ class Forum
     return res.fetch_row[0]
   end
 
-  def topics
-    topics = {} 
-    res = @db.query "select `topic_id`, `topic_title` from `avensis_topics` where `forum_id` = #{@id};"
+  def topic_indexes
+    ids = {} 
+    res = @db.query "select `topic_id` from `avensis_topics` where `forum_id` = #{@id};"
 
-    res.each do |topic|
-      topics[topic[0].to_i] = topic[1]
+    res.each do |row|
+      ids << = row[0]
     end
 
-    topics 
+     ids
+  end
+
+  def topic id
+    Topic.new @db, id
+  end
+end
+
+class Topic
+  @db
+  @id
+
+  def initialize db, id
+    @db = db
+    @id = id
+  end
+
+  def title
+    res = @db.query "select `topic_title` from `avensis_topics` where `topic_id` = #{@id};"
+
+    return res.fetch_row[0]
+  end
+
+  def text
+    res = @db.query "select `topic_text` from `avensis_topics` where `topic_id` = #{@id};"
+
+    return res.fetch_row[0]
   end
 end
 
